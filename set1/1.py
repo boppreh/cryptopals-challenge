ddfa79f2a297bbd093c4d92284167d463c7fb764
BoppreH <boppreh@gmail.com>
2016-10-17 11:27:12 +0200

    Import base64

--- set1/1.py
+++ set1/1.py
@@ -1,4 +1,5 @@
 import math
+from base64 import b64encode, b64decode
 
 hex_chars = '0123456789abcdef'
 
@@ -8,12 +9,12 @@ def get_chars_per_byte(base_chars):
     in the base with the given symbol list.
     """
     chars_per_byte = math.log(2**8, len(base_chars))
-    assert chars_per_byte == int(chars_per_byte)
+    assert chars_per_byte == int(chars_per_byte), chars_per_byte
     return  int(chars_per_byte)
 
 def decode_base(string, base_chars):
     """
-    Decodes the given string in a byte array by using  the given base.
+    Decodes the given string in a byte array by using the given base.
 
         decode_base('01110101', '01')
     """
@@ -32,6 +33,11 @@ def decode_base(string, base_chars):
     return bytes(result)
 
 def encode_base(bytes, base_chars):
+    """
+    Encodes the given bytes in a string using the symbols from `base_chars`.
+
+        encode_base([0xFF], '01')
+    """
     chars_per_byte = get_chars_per_byte(base_chars)
 
     chars = []
@@ -42,9 +48,11 @@ def encode_base(bytes, base_chars):
             b %= power
     return ''.join(chars)
 
-def from_hex(string): return decode_base(string, hex_chars)
-def to_hex(bytes): return encode_base(bytes, hex_chars)
+from_hex = lambda string: decode_base(string, hex_chars)
+to_hex = lambda bytes: encode_base(bytes, hex_chars)
+from_base64 = lambda string: b64decode(string)
+to_base64 = lambda bytes: b64encode(bytes).decode('ascii')
 
 string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 assert to_hex(from_hex(string)) == string
-print(from_hex(string))
+assert to_base64(from_hex(string)) == 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
