63e992fbcc75407f1134d33860c6784e8b8584fa
BoppreH <boppreh@gmail.com>
2016-11-04 15:42:15 +0000

    Fix key generation of 29

--- Set 4: Stream crypto and randomness/29 - Break a SHA-1 keyed MAC using length extension.py
+++ Set 4: Stream crypto and randomness/29 - Break a SHA-1 keyed MAC using length extension.py
@@ -1,6 +1,6 @@
 from utils import *
 
-key = ''.join(str(random_number(0, 10) for i in range(random_number(5, 20)))).encode('ascii')
+key = ''.join(str(random_number(0, 10)) for i in range(random_number(5, 20))).encode('ascii')
 message = b'comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon'
 mac = sha1mac(key, message)
 candidates = extend_sha1(mac, b';admin=true')
