a3342ca08f24fbf0cf506716a8b3339bba76e87d
BoppreH <boppreh@gmail.com>
2016-11-10 14:06:15 +0000

    Add hmac_sha1

--- Set 4: Stream crypto and randomness (incomplete)/31 - Implement and break HMAC-SHA1 with an artificial timing leak.py
+++ Set 4: Stream crypto and randomness (incomplete)/31 - Implement and break HMAC-SHA1 with an artificial timing leak.py
@@ -1,7 +1,12 @@
 from utils import *
 from time import sleep
 
-key = random_bytes(16)
-def test_hmac(
-serve_http(lambda b: print(b) or (200, b'result'))
-print(send_http(b'data'))
+key = random_bytes(20)
+
+# Broken at the moment.
+#serve_http(lambda b: print(b) or (200, b'result'), n_requests=2)
+#print(send_http(b'data'))
+
+assert hmac_sha1(b'', b'') == from_hex('fbdb1d1b18aa6c08324b7d64b71fb76370690e1d')
+assert hmac_sha1(b'key', b'The quick brown fox jumps over the lazy dog') == from_hex('de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9')
+
